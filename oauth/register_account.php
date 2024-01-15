24e2d33657f495e3e271271e7db736ce6c3fe67f
Шамиль Хайрулаев <sa.hairulaev@gmail.com>
2024-01-16 02:32:34 +0300

    Обновил функционал регистрации аккаунта

--- oauth/register_account.php
+++ oauth/register_account.php
@@ -7,14 +7,19 @@ if (!isset($_GET['code']) ||
     !isset($_GET['client_secret']))
     die;
 
-$currentUrl = (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on' ? "https" : "http") . "://" . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'];
+$redirectUri = (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on' ? "https" : "http") . "://" . $_SERVER['HTTP_HOST'].'?client_secret='.$_GET['client_secret'];
 
 $accessTokenData = OauthProcessor::registerAccount($_GET['referer'], [
     'client_id' => $_GET['client_id'],
     'client_secret' => $_GET['client_secret'],
     'code' =>$_GET['code'],
-    'redirect_uri' => $currentUrl
+    'redirect_uri' => $redirectUri
 ]);
 
 $subdomain = explode('.', $_GET['referer'])[0];
 file_put_contents(ACCESS_TOKEN_DIRPATH."/$subdomain.json", $accessTokenData);
+file_put_contents(CLIENT_SECRETS_DIRPATH."/$subdomain.json", json_encode([
+    'client_id' => $_GET['client_id'],
+    'client_secret' => $_GET['client_secret'],
+    'redirect_uri' => $redirectUri
+]));
