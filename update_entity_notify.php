20c7e4cb03eceed1f8b7f4e89d0b489685f1fb3a
Шамиль Хайрулаев <sa.hairulaev@gmail.com>
2024-01-16 19:19:23 +0300

    Checkpoint

--- update_entity_notify.php
+++ update_entity_notify.php
@@ -1,24 +1,35 @@
 <?php
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Headers: *");
 
 require_once __DIR__.'/vendor/autoload.php';
 
-if (!isset($_POST['leads']['add']) &&
-    !isset($_POST['leads']['update']) &&
-    !isset($_POST['contacts']['add']) &&
-    !isset($_POST['contacts']['update']))
+parse_str(file_get_contents('php://input'), $requestData);
+
+if (!isset($requestData['leads']['add']) &&
+    !isset($requestData['leads']['update']) &&
+    !isset($requestData['contacts']['add']) &&
+    !isset($requestData['contacts']['update']))
     die;
 
+$notifyProcessor = new NotifyProcessor($requestData['account']['subdomain']);
 
-
-if (isset($_POST['leads']['add'])) {
-    $leadId = $_POST['leads']['add']['id'];
-    $leadName = $_POST['leads']['add']['name'];
-    $leadAddedTimestamp = $_POST['leads']['add']['last_modified'];
-    $responsibleUserId = $_POST['leads']['add']['responsible_user_id'];
+if (isset($requestData['leads']['add'])) {
+    $leadId = $requestData['leads']['add']['id'];
+    $leadName = $requestData['leads']['add']['name'];
+    $leadAddedTimestamp = $requestData['leads']['add']['last_modified'];
+    $responsibleUserId = $requestData['leads']['add']['responsible_user_id'];
     $responsibleUserName = 'TODO Добавить процесс получения имени'; # todo Добавить процесс получения имени
 
-    $notifyProcessor = new NotifyProcessor($_POST['account']['subdomain']);
     $notifyProcessor->addNotify($leadId, $leadName,'leads', $responsibleUserName, $leadAddedTimestamp);
 }
-//elseif (isset($_POST['leads']['update']))
-//    NotifyProcessor::updateNotify();
+elseif (isset($requestData['contacts']['add'])) {
+    $contactId = $requestData['contacts']['add']['id'];
+    $contactName = $requestData['contacts']['add']['name'];
+    $contactAddedTimestamp = $requestData['contacts']['add']['last_modified'];
+    $responsibleUserId = $requestData['contacts']['add']['responsible_user_id'];
+    $responsibleUserName = 'TODO Добавить процесс получения имени'; # todo Добавить процесс получения имени
+
+    $notifyProcessor->addNotify($contactId, $contactName, 'contacts', $responsibleUserName, $contactAddedTimestamp);
+}
+//elseif (isset($requestData['leads']['update']))
